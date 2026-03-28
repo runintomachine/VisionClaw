13f6620b5cc2ce6abd1eb7a0b22b3f965508075d
RITM X <ritmx@MacBook-Pro.local>
2026-03-28 11:24:42 +0100

    Add WebRTC signaling URL

---
+++ samples/CameraAccess/CameraAccess/Secrets.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+enum Secrets {
+  static let geminiAPIKey = "AIzaSyBpl-FscZ_-S__R-qzpHmA8TZTjhJPY1o4"
+  static let openClawHost = "http://MacBook-Pro.local"
+  static let openClawPort = 18789
+  static let openClawHookToken = "YOUR_OPENCLAW_HOOK_TOKEN"
+  static let openClawGatewayToken = "YOUR_OPENCLAW_GATEWAY_TOKEN"
+  static let webrtcSignalingURL = "ws://172.20.10.5:8080"
+}
